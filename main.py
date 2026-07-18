9e5f15fc369747577b146c99b9d3a1939af7ba48
Arthur <103148113+Arthur0li@users.noreply.github.com>
2026-07-18 00:51:11 +0000

    feat: adding columns and graphics

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import seaborn as sns
 
 df = pd.read_csv("world_cup_data.csv")
 
-print(df.head())
+df["Goals"] = df["Top Team Scorer"].str.extract(r"(\d+)$").astype(int)
+df["Player"] = df["Top Team Scorer"].str.replace(r"\s*-\s*\d+$", "", regex=True)
+
+goals = df.sort_values(by="Goals", ascending=False)
+
+plt.figure(figsize=(10,5))
+sns.barplot(data=goals, x="Goals", y="Top Team Scorer", hue="Squad")
+plt.show()
